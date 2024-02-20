1f4050ae7baa562f93221b1dd96972ba0d850032
Iraponan Marinho <iraponan.aires@gmail.com>
2024-02-19 21:59:04 -0300

    Criando a tela de login.

---
+++ lib/pages/login/login.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            Stack(
+              children: [
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 70, bottom: 32),
+                      child: Image.asset(
+                        'assets/images/tickicon.png',
+                        width: 150,
+                        height: 150,
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/login/widgets/form_container.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class FormContainer extends StatelessWidget {
+  const FormContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20),
+      child: Form(
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
